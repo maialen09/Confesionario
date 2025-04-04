2dd3cdd82a14209cb7d1aee37ddc4390cb487d64
Maialen Torre <maialentorre09@gmail.com>
2025-04-04 19:13:05 +0200

    comprobaciones de usuario/contraseña vacias

--- static/js/registro.js
+++ static/js/registro.js
@@ -18,7 +18,41 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   
-    // Verifica cada vez que el usuario escribe en uno de los campos
     pass1.addEventListener("input", comprobarCoincidencia);
     pass2.addEventListener("input", comprobarCoincidencia);
+
+    const btnReg = document.getElementById("botonRegistrarse");
+  
+    btnReg.addEventListener("click", function() {
+
+      // comprobar que no son null ni el usuario ni la contraseña xxxxxxx
+      // hacer que la contraseña sea segura y de la longitud adecuada 
+      // comprobar que el nombre de usuario no exista ya y que sea de la longitud adecuada
+      // avisar si las contraseñas no coinciden 
+      // añadir en la base de datos al usuario 
+
+      const textoUsuario = document.getElementById("textUsuario");
+      const textoContrasena = document.getElementById("textContrasena"); 
+
+      const user = textoUsuario.value.trim(); 
+      const contrasena = textoContrasena.value.trim(); 
+
+      if (!user) {  
+        alert("El nombre de usuario está vacío o contiene solo espacios.");
+    }
+
+    if (!contrasena) {  
+       alert("La contraseña está vacía o contiene solo espacios.");
+    }
+
+
+
+
+
+      
+
+
+
+
+    });
   });  
