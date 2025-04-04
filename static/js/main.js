3fd2aac8751b0ceb9ca2411337480129bf8be35f
mikel gojenola <mikelgojenola002@gmail.com>
2025-04-04 18:35:34 +0200

    Creación html registro

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el botón
+    const btnIr = document.getElementById("botonRegi");
+  
+    // Asignar el evento de clic al botón
+    btnIr.addEventListener("click", function() {
+      // Cambiar a otra página cuando el botón sea clickeado
+      window.location.href = "registro.html"; // Redirige a otra página
+      // O si prefieres abrir en una nueva ventana:
+      // window.open("otra_pagina.html", "_blank");
+    });
+  });
+  
