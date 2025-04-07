c04017db93fed5cf9c10f00e2d80c2a0b0d931dd
mikel gojenola <mikelgojenola002@gmail.com>
2025-04-07 18:09:53 +0200

    Insertar usuario, y comprobaciones de registro

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+db_config = {
+    'user': 'admin',
+    'password': 'calavera',
+    'host': 'db',
+    'database': 'Confesionario',
+    'port': '3306'
+}
+
+def anadir_usuario(user, contrasena):
+    with mysql.connector.connect(**db_config) as conn:
+        cursor.conn.cursor()
+        query = "SELECT COUNT(*) FROM Usuarios WHERE nombre = %s"
+        cursor.execute(query, (user,))
+        count = cursor.fetchone()[0]
+        if(count == 0):
+            query = "INSERT INTO Usuarios(nombre, contrasena) VALUES(%s, %s)"
+            cursor.execute(query, (nombre, contrasena))
+            conn.commit()
+            return True
+        else:
+            return False
 
 @app.route('/')
 def index():
     return render_template('main.html')  # Esto buscará el archivo en /templates/index.html
 
+@app.route('/insertar_usuario')
+def insertar_usuario():
+    user = request.json['user']
+    contrasena = request.json['contrasena']
+    existe = anadir_usuario(user, contrasena)
+    return jsonify({"existe": existe})
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
--- static/js/registro.js
+++ static/js/registro.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const pass2 = document.getElementById("textContrasena2");
     const mensaje = document.getElementById("mensaje");
   
+    var coincide = false;
     function comprobarCoincidencia() {
       if (pass1.value === "" || pass2.value === "") {
         mensaje.textContent = ""; // No mostrar nada si alguno está vacío
@@ -12,9 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
       if (pass1.value === pass2.value) {
         mensaje.textContent = "✅ Las contraseñas coinciden";
         mensaje.style.color = "green";
+        coincide = true;
       } else {
         mensaje.textContent = "❌ Las contraseñas no coinciden";
         mensaje.style.color = "red";
+        coincide = false;
       }
     }
   
@@ -39,14 +42,47 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (!user) {  
         alert("El nombre de usuario está vacío o contiene solo espacios.");
-    }
+      }
 
-    if (!contrasena) {  
-       alert("La contraseña está vacía o contiene solo espacios.");
-    }
+      if (!contrasena) {  
+        alert("La contraseña está vacía o contiene solo espacios.");
+      }
+      else {
+        const longitudValidaCon = contrasena.length >= 8 && contrasena.length < 100;
+        const longitudValidaUsuario = user.length >= 8 && user.length < 100;
+        const tieneMayus = /[A-Z]/.test(contrasena);
+        const tieneNum = /\d/.test(contrasena);
 
+        if(!tieneNum){
+          alert("Falta número");
+        }
+        if(!tieneMayus){
+          alert("Falta mayuscula");
+        }
+        if(!longitudValidaCon){
+          alert("Pon una contraseña de entre 8 y 100 caracteres");
+        }
+        if(!longitudValidaUsuario){
+          alert("Pon un usuario de entre 8 y 100 caracteres");
+        }
+        if(!coincide){
+          alert("Las contraseñas no coinciden idiota");
+        }
+      }
 
-
+      fetch('/insertar_usuario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user: user, contrasena: contrasena})
+      })
+      .then(response => response.json())
+      .then(data => {
+        const existe = data.existe;
+        console.log("El usuario " + existe)
+      })
+      .catch(error => console.error('Error al obtener el nombre de usuario'))
 
 
       
